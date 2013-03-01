ca87961e2a1abd89493bfe36bfad3303682a8149
Leonardo Boshell <leonardo@diptongonante.com>
2013-02-28 21:56:09 -0500

    Added basic drawing of boot

--- khans-boots.js
+++ khans-boots.js
@@ -17,7 +17,7 @@
 //
 // $
 //
-// Global object, used to store all sort of things from the game
+// Global object, used to store all sort of things for the game
 //
 var $ = {};
 
@@ -64,14 +64,21 @@ $.room = [];
  *************/
 
 //
+// Utility function to implement inheritance
+//
+$.extend = function(obj, from) {
+	for (var p in from) { obj[p] = from[p]; }
+};
+
+//
 // KhanObject
 //
 // The basic class from which most other objects inherit.
 //
 var KhanObject = function() {};
-KhanObject.prototype = {
+$.extend(KhanObject.prototype, {
 	x : 0, y : 0
-};
+});
 
 //
 // Hand
@@ -79,53 +86,56 @@ KhanObject.prototype = {
 // The "cursor" with which you interact in the world
 //
 var Hand = function() {};
-Hand.prototype = {
+Hand.prototype = new KhanObject();
+Hand.prototype.constructor = Hand;
+
+$.extend(Hand.prototype, {
 	vx : 0, vy : 0,
 	ax : 0, ay : 0,
 
-	w : 0, h : 0,
+	w : 0, h : 0
+});
 
-	onInit : function() {
-		this.w = this.h = $.cfg.u * 2;
-		this.x = (width - this.w) / 2;
-		this.y = (height - this.h) / 2;
-		this.kol = $.colors.red[0] | 0xdd000000;
-	},
+Hand.prototype.onInit = function() {
+	this.w = this.h = $.cfg.u * 2;
+	this.x = (width - this.w) / 2;
+	this.y = (height - this.h) / 2;
+	this.kol = $.colors.red[0] | 0xaa000000;
+};
 
-	onFrame : function() {
-		if (this.ax !== 0) { this.vx += this.ax; }
-		if (this.ay !== 0) { this.vy += this.ay; }
+Hand.prototype.onFrame = function() {
+	if (this.ax !== 0) { this.vx += this.ax; }
+	if (this.ay !== 0) { this.vy += this.ay; }
 
-		this.vx = constrain(this.vx, -$.cfg.speedMax, $.cfg.speedMax);
-		this.vy = constrain(this.vy, -$.cfg.speedMax, $.cfg.speedMax);
+	this.vx = constrain(this.vx, -$.cfg.speedMax, $.cfg.speedMax);
+	this.vy = constrain(this.vy, -$.cfg.speedMax, $.cfg.speedMax);
 
-		this.x += this.vx;
-		this.y += this.vy;
+	this.x += this.vx;
+	this.y += this.vy;
 
-		this.x = constrain(this.x, 0, width - this.w);
-		this.y = constrain(this.y, 0, height - this.h);
-	},
+	this.x = constrain(this.x, 0, width - this.w);
+	this.y = constrain(this.y, 0, height - this.h);
+};
 
-	onDraw : function() {
-		noStroke();
-		fill(this.kol);
-		rect(this.x, this.y, this.w, this.h);
-	},
+Hand.prototype.onDraw = function() {
+	noStroke();
+	fill(this.kol);
+	rect(this.x, this.y, this.w, this.h);
+};
 
-	onKeyPressed : function(k, kc) {
-		if (kc === RIGHT) { this.ax = $.cfg.accel; }
-		else if (kc === LEFT) { this.ax = -$.cfg.accel; }
-		else if (kc === UP) { this.ay = -$.cfg.accel; }
-		else if (kc === DOWN) { this.ay = $.cfg.accel; }
-	},
+Hand.prototype.onKeyPressed = function(k, kc) {
+	if (kc === RIGHT) { this.ax = $.cfg.accel; }
+	else if (kc === LEFT) { this.ax = -$.cfg.accel; }
+	else if (kc === UP) { this.ay = -$.cfg.accel; }
+	else if (kc === DOWN) { this.ay = $.cfg.accel; }
+};
 
-	onKeyReleased : function(k, kc) {
-		if (kc === RIGHT || kc === LEFT) {
-			this.ax = 0; this.vx = 0;
-		}
-		else if (kc === UP || kc === DOWN) {
-			this.ay = 0; this.vy = 0;
-		}
+Hand.prototype.onKeyReleased = function(k, kc) {
+	if (kc === RIGHT || kc === LEFT) {
+		this.ax = 0; this.vx = 0;
+	}
+	else if (kc === UP || kc === DOWN) {
+		this.ay = 0; this.vy = 0;
 	}
 };
 
@@ -135,8 +145,22 @@ Hand.prototype = {
 // A boot. It kicks things.
 //
 var Boot = function() {};
-Boot.prototype = {
-	
+Boot.prototype = new KhanObject();
+Boot.prototype.constructor = Boot;
+
+Boot.prototype.onInit = function() {
+	this.x = width / 3;
+	this.y = height / 2;
+
+	this.kol = $.colors.brown[0] | 0xff000000;
+};
+
+Boot.prototype.onDraw = function() {
+	noStroke();
+	fill(this.kol);
+	rect(this.x, this.y, $.cfg.u * 2, $.cfg.u * 3);
+	arc(this.x + $.cfg.u * 2.5, this.y + $.cfg.u * 3,
+	    $.cfg.u * 3, $.cfg.u * 3, 180, 360);
 };
 
 
@@ -144,8 +168,11 @@ Boot.prototype = {
  ** Functions **
  ***************/
 
-$.main = function() {
-	$.room.push(new Hand());
+//
+// Entry point for the program
+//
+$.kickIt = function() {
+	$.room.push(new Boot(), new Hand());
 
 	$.room.forEach(function(obj) {
 		if (typeof(obj.onInit) === 'function') { obj.onInit(); }
@@ -164,6 +191,9 @@ $.main = function() {
 	});
 };
 
+//
+// Processing.JS callback for "draw" event
+//
 var draw = function() {
 	$.callbacks.frame.forEach(function(obj) { obj.onFrame(); });
 
@@ -172,6 +202,9 @@ var draw = function() {
 	$.callbacks.draw.forEach(function(obj) { obj.onDraw(); });
 };
 
+//
+// Processing.JS callback for "keyPressed" event
+//
 var keyPressed = function() {
 	$.callbacks.keyPressed.forEach(function(obj) {
 		obj.onKeyPressed(key, keyCode);
@@ -179,10 +212,16 @@ var keyPressed = function() {
 
 };
 
+//
+// Processing.JS callback for "keyReleased" event
+//
 var keyReleased = function() {
 	$.callbacks.keyReleased.forEach(function(obj) {
 		obj.onKeyReleased(key, keyCode);
 	});
 };
 
-$.main();
+//
+// Let's start this party...
+//
+$.kickIt();
