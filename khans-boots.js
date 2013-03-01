d3788856343e4b357d98746b38e13b4ffd8be1eb
Leonardo Boshell <leonardo@diptongonante.com>
2013-02-28 19:57:57 -0500

    First commit.
    So far it only contains a very short skeleton for the first program: Khan's
    Boots, which only has the logic for moving the "hand".

---
+++ khans-boots.js
@@ -0,0 +1,188 @@
+/**
+ * Khan's Boots
+ * ============
+ * A simple game based on the classic "Rocky's Boots", created for the Khan
+ * Academy Computer Science platform.
+ *
+ * Currently a work in progress.
+ *
+ * This is released to the public domain. Feel free to use it as you please.
+ */
+
+
+/*************
+ ** Objects **
+ *************/
+
+//
+// $
+//
+// Global object, used to store all sort of things from the game
+//
+var $ = {};
+
+//
+// Configuration
+//
+$.cfg = {
+	accel    : 1,          // acceleration, in pixels/frame^2
+	speedMax : 15,         // maximum speed, in pixels/frame
+	u        : width / 32  // basic "length" unit in pixels
+};
+
+//
+// Colors, based on the Tango Icon Theme Guidelines
+//
+$.colors = {
+	yellow : [ 0xfce94f, 0xedd400, 0xc4a000 ],
+	orange : [ 0xfcaf3e, 0xf57900, 0xce5c00 ],
+	brown  : [ 0xe9b96e, 0xc17d11, 0x8f5902 ],
+	green  : [ 0x8ae234, 0x73d216, 0x4e9a06 ],
+	blue   : [ 0x729fcf, 0x3465a4, 0x204a87 ],
+	purple : [ 0xad7fa8, 0x75507b, 0x5c3566 ],
+	red    : [ 0xef2929, 0xcc0000, 0xa40000 ]
+};
+
+//
+// Objects that are able to listen to different events
+//
+$.callbacks = {
+	frame       : [],
+	draw        : [],
+	keyPressed  : [],
+	keyReleased : []
+};
+
+//
+// A room
+//
+$.room = [];
+
+
+/*************
+ ** Classes **
+ *************/
+
+//
+// KhanObject
+//
+// The basic class from which most other objects inherit.
+//
+var KhanObject = function() {};
+KhanObject.prototype = {
+	x : 0, y : 0
+};
+
+//
+// Hand
+//
+// The "cursor" with which you interact in the world
+//
+var Hand = function() {};
+Hand.prototype = {
+	vx : 0, vy : 0,
+	ax : 0, ay : 0,
+
+	w : 0, h : 0,
+
+	onInit : function() {
+		this.w = this.h = $.cfg.u * 2;
+		this.x = (width - this.w) / 2;
+		this.y = (height - this.h) / 2;
+		this.kol = $.colors.red[0] | 0xdd000000;
+	},
+
+	onFrame : function() {
+		if (this.ax !== 0) { this.vx += this.ax; }
+		if (this.ay !== 0) { this.vy += this.ay; }
+
+		this.vx = constrain(this.vx, -$.cfg.speedMax, $.cfg.speedMax);
+		this.vy = constrain(this.vy, -$.cfg.speedMax, $.cfg.speedMax);
+
+		this.x += this.vx;
+		this.y += this.vy;
+
+		this.x = constrain(this.x, 0, width - this.w);
+		this.y = constrain(this.y, 0, height - this.h);
+	},
+
+	onDraw : function() {
+		noStroke();
+		fill(this.kol);
+		rect(this.x, this.y, this.w, this.h);
+	},
+
+	onKeyPressed : function(k, kc) {
+		if (kc === RIGHT) { this.ax = $.cfg.accel; }
+		else if (kc === LEFT) { this.ax = -$.cfg.accel; }
+		else if (kc === UP) { this.ay = -$.cfg.accel; }
+		else if (kc === DOWN) { this.ay = $.cfg.accel; }
+	},
+
+	onKeyReleased : function(k, kc) {
+		if (kc === RIGHT || kc === LEFT) {
+			this.ax = 0; this.vx = 0;
+		}
+		else if (kc === UP || kc === DOWN) {
+			this.ay = 0; this.vy = 0;
+		}
+	}
+};
+
+//
+// Boot
+//
+// A boot. It kicks things.
+//
+var Boot = function() {};
+Boot.prototype = {
+	
+};
+
+
+/***************
+ ** Functions **
+ ***************/
+
+$.main = function() {
+	$.room.push(new Hand());
+
+	$.room.forEach(function(obj) {
+		if (typeof(obj.onInit) === 'function') { obj.onInit(); }
+		if (typeof(obj.onFrame) === 'function') {
+			$.callbacks.frame.push(obj);
+		}
+		if (typeof(obj.onDraw) === 'function') {
+			$.callbacks.draw.push(obj);
+		}
+		if (typeof(obj.onKeyPressed) === 'function') {
+			$.callbacks.keyPressed.push(obj);
+		}
+		if (typeof(obj.onKeyReleased) === 'function') {
+			$.callbacks.keyReleased.push(obj);
+		}
+	});
+};
+
+var draw = function() {
+	$.callbacks.frame.forEach(function(obj) { obj.onFrame(); });
+
+	background(255, 255, 255);
+
+	$.callbacks.draw.forEach(function(obj) { obj.onDraw(); });
+};
+
+var keyPressed = function() {
+	$.callbacks.keyPressed.forEach(function(obj) {
+		obj.onKeyPressed(key, keyCode);
+	});
+
+};
+
+var keyReleased = function() {
+	$.callbacks.keyReleased.forEach(function(obj) {
+		obj.onKeyReleased(key, keyCode);
+	});
+};
+
+$.main();
